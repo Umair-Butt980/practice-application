9c0f83a54d662f5f626c634192e801e92b7d4887
UmairButt <umairamjadbutt1@gmail.com>
2022-10-20 23:27:48 +0500

    First components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> HEllo this is the me </h1>
+        <ExpenseItem />
       </header>
     </div>
   );
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExpenseItem() {
+  return <h1> Hello this is expense conponent</h1>;
+}
+
+export default ExpenseItem;
