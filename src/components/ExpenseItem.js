c9845b57d1dad1c7ee48e24dbdd1355672b5e4e4
UmairButt <umairamjadbutt1@gmail.com>
2022-10-27 10:37:23 +0500

    component with styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div> Date </div>
-      <div> Title </div>
-      <div> Amount </div>
+    <div className="expense-item">
+      <div> October 27th 2022 </div>
+      <div className="expense-item__description">
+        <h2> Insurance </h2>
+      </div>
+      <div className="expense-item__price">$202.12</div>
     </div>
   );
 }
