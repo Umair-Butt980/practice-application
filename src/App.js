f9fe123238b0057568b8dfde5bb4dc66e0fc57cd
UmairButt <umairamjadbutt1@gmail.com>
2023-04-03 00:38:59 +0500

    rendered a list plus conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+ const dummy_expenses = [
+   {
+     id: "e1",
+     title: "Toilet Paper",
+     amount: 94.12,
+     date: new Date(2020, 7, 14),
+   },
+   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+   {
+     id: "e3",
+     title: "Car Insurance",
+     amount: 294.67,
+     date: new Date(2021, 2, 28),
+   },
+   {
+     id: "e4",
+     title: "New Desk (Wooden)",
+     amount: 450,
+     date: new Date(2021, 5, 12),
+   },
+ ];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+ const [expenses, setExpenses] = useState(dummy_expenses)
 
   const onAddExpenseHandler = (expense) =>{
-    console.log(expense, "in the app js file")
+    setExpenses((prevExpenses)=>{
+       return [expense , ...prevExpenses]
+    })
+    console.log(expenses, expense , "<+++++?");
   }
   return (
     <div className="App">
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -18,7 +18,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={updateTitleHandler}>Update Title</button>
+      {/* <button onClick={updateTitleHandler}>Update Title</button> */}
     </Card>
   );
 };
