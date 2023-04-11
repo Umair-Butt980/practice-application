12db329f05ebbb80f3f61e88fd4866bd0a53735a
UmairButt <umairamjadbutt1@gmail.com>
2023-04-11 12:06:40 +0500

    added the logic for filtering the items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,7 +7,13 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-   
+   const filterChangeHandler = (selectedYear) => {
+     setFilteredYear(selectedYear);
+   };
+
+   const filteredExpenses = props.items.filter((expense) => {
+     return expense.date.getFullYear().toString() === filteredYear;
+   });
 
   return (
     <Card className="expenses">
