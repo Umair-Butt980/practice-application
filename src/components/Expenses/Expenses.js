f23a7611da2467a92d260d284907d7a75637e3bb
UmairButt <umairamjadbutt1@gmail.com>
2023-06-15 15:27:23 +0500

    cleaning up the consoles and code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
@@ -8,17 +7,8 @@ import ExpenseList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  //derived state concept
-  // let filterdInfo = '2019, 2020 and 2021'
-  // if (setFilteredYear === "2020") {
-  //   filterdInfo = "2019, 2021 and 2022";
-  // } else if (setFilteredYear === "2021") {
-  //   filterdInfo = "2020, 2019 and 2022";
-  // } else if (setFilteredYear === "2022") {
-  //   filterdInfo = "2020, 2021 and 2023";
-  // }
+  
    const filterChangeHandler = (selectedYear) => {
-    // console.log('im called from the child component')
      setFilteredYear(selectedYear);
    };
    
