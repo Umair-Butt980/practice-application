0d1baf9cb8af8f533463d73a62848d3d0f1d387a
UmairButt <umairamjadbutt1@gmail.com>
2023-06-20 12:10:21 +0500

    new chart component Plus css added

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChartBar from "./Chartbar";
+import './Chart.css'
+
+const Chart = props =>{
+    return (
+      <div className="chart">
+        {props.dataPoints.map((dataPoint) => (
+          <ChartBar value={dataPoint.value} maxValue={null} label={dataPoint.label} key={dataPoint.label} />
+        ))}
+      </div>
+    );
+}
+
+export default Chart;
